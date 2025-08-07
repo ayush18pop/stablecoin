e7df5057c86d5c39d52a884ab95c0a2f517280d8
ayush18pop <ayushsp05@gmail.com>
2025-08-07 16:46:08 +0530

    Add initial project structure with CI workflow, README, and configuration files

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/*
+ * @title Decentralized Stable Coin
+ * @author Ayush Popat
+ */
+contract DecentralizedStableCoin {
+    constructor() {
+        // Initialization logic can go here if needed
+    }
+}
